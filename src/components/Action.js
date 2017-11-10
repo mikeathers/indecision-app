9bb385e2f02b084aea6c2aee2aba43d1dedf85a7
mikeathers <athers_05@hotmail.co.uk>
2017-11-10 07:35:52 +0000

    Added styles to the project.

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Action = (props) => (
     <div>
-        <button onClick={props.handlePick}
-            disabled={!props.hasOptions}>What should i do?</button>
+        <button 
+            className="big-button"
+            onClick={props.handlePick}
+            disabled={!props.hasOptions}>What should i do?
+        </button>
     </div>
 );
 
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 const Option = (props) => (
-  <div>
-    {props.optionText}
+  <div className="option">
+  <p className="option__text">{props.count}. {props.optionText}</p>    
     <button
+      className="button button--link"
       onClick={() => {
         props.handleDeleteOption(props.optionText);
       }}
     >
-      remove
+      Remove
       </button>
   </div>
 );
