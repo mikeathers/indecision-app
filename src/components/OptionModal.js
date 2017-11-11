6f4a500e2dcd1de98413e5751276e0f78a2f9da8
mikeathers <athers_05@hotmail.co.uk>
2017-11-11 09:40:24 +0000

    Completed app, added mobile-first css

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption} // Converts values to a boolean value.
     onRequestClose={props.handleClearSelectedOption} // Allows escape or clicking background
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
 );
 
